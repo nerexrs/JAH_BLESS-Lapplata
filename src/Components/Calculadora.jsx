70bbc5cbf055362e4df65c3ed1a042f3802efc37
samu-shaper <samu@shaper.us>
2024-04-22 03:12:53 -0500

    JAH BLESS, subiendo primer commit de la versión 0.0.0.0.1 xd de lapplata, modal, funcion calculadora para - y + y una tabla donde después se verán los datos

--- src/Components/Calculadora.jsx
+++ src/Components/Calculadora.jsx
@@ -11,10 +11,14 @@ const Calculadora = () => {
         <span>{number}</span>
 
         <input type="number" className=' text-whit bg-gray-800 text-center' value={number} onChange={(e) => setNumber(parseInt(e.target.value))}/>
-        <div className='flex flex-row gap-4'>
+        <div className='flex flex-row gap-11'>
             <button onClick={(e) => setTotal(total - number)}>-</button>
             <button onClick={(e) => setTotal(total + number)}>+</button>
         </div>
+            <div className='flex flex-row gap-4'>
+                <p>Pulsame para restar al total el valor ingresado</p>
+                <p>Pulsame para sumar al total el valor ingresado</p>
+            </div>
         <h6>Tu total es {total}</h6>
     </div>
   )
